28922d7e4c66663d16044969a9afa03d0ee98b5e
rjvince <rob.vince@gmail.com>
2024-07-01 12:29:11 -0400

    New file format to prep for transposing

--- src/main/java/com/rjvince/ChordDiagram.java
+++ src/main/java/com/rjvince/ChordDiagram.java
@@ -4,36 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChordDiagram {
+    private Note root;
+    private String chordType;
     private String name;
     private int startFret;
     private List<Integer> fingers;
     private List<String> noteNames;
-    private Note [] tuning = {Note.G, Note.C, Note.E, Note.A};
+    private Note[] tuning = {Note.G, Note.C, Note.E, Note.A};
     private boolean sharps;
-    private String fontFamily = "'Alata-Regular', 'Alata'";
-
-    public String getFontFamily() {
-        return fontFamily;
-    }
-
-    public void setFontFamily(String fontFamily) {
-        this.fontFamily = fontFamily;
-    }
-
 
     public ChordDiagram(String row) {
-        String [] tokens = row.split(",");
-        name = tokens[0].trim();
-        startFret = Integer.parseInt(tokens[1].trim());
+        String[] tokens = row.split(",");
+        root = Note.valueOf(tokens[0]);
+        chordType = tokens[1];
+        startFret = Integer.parseInt(tokens[2].trim());
 
         fingers = new ArrayList<>(4);
-        for( String finger : tokens[2].trim().split("-")) {
+        for (String finger : tokens[3].trim().split("-")) {
             fingers.add(Integer.parseInt(finger));
         }
 
         sharps = false;
-        if (tokens.length == 4) {
-            sharps = tokens[3].trim().equals("sharps");
+        if (tokens.length == 5) {
+            sharps = tokens[4].trim().equals("sharps");
         }
 
         int offset = startFret == 0 ? 0 : startFret - 1;
@@ -42,13 +35,14 @@ public class ChordDiagram {
         noteNames.add(formatNote(tuning[1].shiftUp(offset + fingers.get(1))));
         noteNames.add(formatNote(tuning[2].shiftUp(offset + fingers.get(2))));
         noteNames.add(formatNote(tuning[3].shiftUp(offset + fingers.get(3))));
+
+        name = formatNote(root) + chordType;
     }
 
     private String formatNote(Note n) {
         if (sharps) {
             return n.useSharps().replace('s', '♯');
-        }
-        else {
+        } else {
             return n.useFlats().replace('f', '♭');
         }
     }
@@ -61,7 +55,6 @@ public class ChordDiagram {
                 this.fingers.get(0), this.fingers.get(1),
                 this.fingers.get(2), this.fingers.get(3), String.join(",", this.noteNames));
     }
-
 
 
     //============================================================
